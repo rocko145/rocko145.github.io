3472f808130f269389fcb8000982fd655071694a
rocko145 <mac@Macs-MacBook-Pro-94.local>
2015-03-10 23:53:31 -0400

    up 1.0

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -17,3 +17,24 @@ $(function() {
         event.preventDefault();
     });
 });
+
+function initialize() {
+
+    var mapCanvas = document.getElementById('map-canvas');
+    var myLatlng = new google.maps.LatLng(50.588539, 30.495565);
+    var mapOptions = {
+      center: myLatlng,
+      zoom: 14,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    
+    var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: 'Hello World!'
+    });
+
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
